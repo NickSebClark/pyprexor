8e37bbdd2939f748451e01b894cb340a56c4b19f
_MircoWave_ <clarknick1990@gmail.com>
2023-08-25 17:27:55 +0100

    Added a quick test for coverage.

---
+++ pyprexor/tests/test_datastore.py
@@ -0,0 +1,12 @@
+import pyprexor.datastore as datastore
+import pytest
+
+
+def test_abstract_datastore():
+    store = datastore.Datastore()
+
+    with pytest.raises(NotImplementedError):
+        store.get_parameter_set(1)
+
+    with pytest.raises(NotImplementedError):
+        store.write_process_data({})
